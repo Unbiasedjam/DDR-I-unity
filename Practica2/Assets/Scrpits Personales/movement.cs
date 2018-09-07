c7aad0111ee5f1b07523bec9394a45789d368d32
Ivan Quevedo <ivan.quevedo@uabc.edu.mx>
2018-09-07 15:40:20 -0700

    Practica 2 Terminada
    Terminado el menu de pausa, el movimiento de la camra sigue al personaje y la estetica de la pantalla principal esta lista (mas un avanze de la siguiente practica)

--- Practica2/Assets/Scrpits Personales/movement.cs
+++ Practica2/Assets/Scrpits Personales/movement.cs
@@ -1,29 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class movement : MonoBehaviour {
+public class CameraController : MonoBehaviour {
 
-	public float speed;
-	public Rigidbody rb;
-	
+    public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		rb = GetComponent<Rigidbody>();
+    private Vector3 offset;
 
-	}
-
-	 void FixedUpdate (){
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
+    void Start ()
+    {
+        offset = transform.position - player.transform.position;
     }
-
-	
-	// Update is called once per frame
-	//void Update () {}
+    
+    void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+    }
 }
---
+++ Practica2/Assets/interactive.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactive : MonoBehaviour {
+	// Use this for initialization
+	
+	public Color color;
+	private bool isPlayerInside = false;
+	public GameObject infoPanel;
+
+
+	public void Start(){
+		infoPanel.SetActive(false);
+	}
+	public void Update(){
+
+		transform.Rotate(Vector3.up * Time.deltaTime );
+
+		if(Input.GetKeyDown(KeyCode.I) && isPlayerInside){
+			gameObject.GetComponent<Renderer>().material.color = color;
+		}
+	}
+
+	public void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Player")){
+			//this.gameObject.GetComponent<Renderer>().material.color = color;
+			infoPanel.SetActive(true);
+			isPlayerInside = true;
+		}
+		
+	}
+
+	public void OnTriggerExit(Collider other){
+		if(other.CompareTag("Player")){
+			infoPanel.SetActive(false);
+			isPlayerInside = false;
+		}
+	}
+}
