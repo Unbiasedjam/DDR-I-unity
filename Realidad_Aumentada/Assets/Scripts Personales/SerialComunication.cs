23ee6c6dacf32102046850a87d38c0f7d2351ea8
Unknown <ivan.quevedo@uabc.edu.mx>
2018-12-01 10:08:20 -0800

    final
    Las ultimas cosas movidas

---
+++ Realidad_Aumentada/Assets/Scripts Personales/SerialComunication.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+public class SerialComunication : MonoBehaviour {
+
+	SerialPort serial;
+	public string port = "COM3";
+	public int baudRate = 115200;
+
+	private void Awake(){
+		serial = new SerialPort(port, baudRate);
+		serial.Open();
+		Debug.Log("Serial Port abierto");
+	}
+
+	private void Update(){
+		if(Input.GetKeyDown(KeyCode.O)){
+			Write("LED_ON");
+		}
+		else if(Input.GetKeyDown(KeyCode.P)){
+			Write("LED_OFF");
+		}
+	}
+
+	public void Write(string command){
+		if(serial.IsOpen){
+			serial.Write(command);
+		}
+	}
+
+	public void OnAplicationQuit(){
+		serial.Close();
+		Debug.Log("Serial Port cerrado");
+	}
+}
